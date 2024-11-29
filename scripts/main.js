890998738f2a67dbf1888410011214f9ffaba470
Edwxn <edwinpena0306@gmail.com>
2024-11-29 17:48:28 -0400

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,21 @@
+const body = document.getElementsByTagName('body')[0];
 const imgBg = document.getElementById('imgBg');
 const personalName = document.getElementById('personalName');
-const following = document.getElementById('followingText');
-const followers = document.getElementById('followersText');
+const followingText = document.getElementById('followingText');
+const followersText = document.getElementById('followersText');
 const btnFollow = document.getElementById('btnFollow');
 const btnMessage = document.getElementById('btnMessage');
 
 function changeTheme(color) {
     imgBg.style.backgroundColor = color;
     personalName.style.color = color;
-    following.style.color = color;
-    followers.style.color = color;
+    followingText.style.color = color;
+    followersText.style.color = color;
     btnFollow.style.backgroundColor = color;
     btnMessage.style.color = color;
     btnMessage.style.borderColor = color;
 }
+
+setTimeout(() => {
+    body.style.opacity = '1';
+}, 1000);
